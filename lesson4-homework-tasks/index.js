35aa91e271d6b02d8a0fd3db482e675eaf691046
olegtymo <oleg_tymo@ukr.net>
2023-04-03 18:59:30 +0300

    Was added code from 5th webinar

--- lesson4-homework-tasks/index.js
+++ lesson4-homework-tasks/index.js
@@ -9,61 +9,63 @@
 // };
 
 // #################################   TASK #3   ###################################
-let arr = ['house', 'color', 'game', 'car', 'museum'];
+// let arr = ['house', 'color', 'game', 'car', 'museum'];
 
-function createRandElement(arr) {
-  let rand = Math.floor(Math.random() * arr.length);
-  return arr[rand];
-}
+// function createRandElement(arr) {
+//   let rand = Math.floor(Math.random() * arr.length);
+//   return arr[rand];
+// }
 
-let randWord = createRandElement(arr);
-console.log(randWord);
-const guessedLetter = (randWord, letterFromUser) => {
-  let string = '_'.repeat(randWord.length);
-  let updatedString = '';
+// let randWord = createRandElement(arr);
+// console.log(randWord);
+// const guessedLetter = (randWord, letterFromUser) => {
+//   let string = '_'.repeat(randWord.length);
+//   let updatedString = '';
 
-  for (let i = 0; i < randWord.length - 1; i++) {
-    if (randWord[i] === letterFromUser) {
-      updatedString += randWord[i];
-    }
-    updatedString += `${string[i]} `;
-  }
-  return updatedString;
-};
+//   for (let i = 0; i < randWord.length - 1; i++) {
+//     if (randWord[i] === letterFromUser) {
+//       updatedString += randWord[i];
+//     }
+//     updatedString += `${string[i]} `;
+//   }
+//   return updatedString;
+// };
 
-function isCompatible(letterFromUser, randWord) {
-  let lowCaseLetter = letterFromUser.toLowerCase();
-  let lowCaseWord = randWord.toLowerCase();
-  console.log(lowCaseWord.indexOf(lowCaseLetter));
-  let mistakesCounter = 0;
+// function isCompatible(letterFromUser, randWord) {
+//   let lowCaseLetter = letterFromUser.toLowerCase();
+//   let lowCaseWord = randWord.toLowerCase();
+//   console.log(lowCaseWord.indexOf(lowCaseLetter));
+//   let mistakesCounter = 0;
 
-  while (lowCaseWord.indexOf(lowCaseLetter) === -1 && mistakesCounter < 3) {
-    mistakesCounter++;
-    letterFromUser = prompt(`Nope, try again! You have ${4 - mistakesCounter} attempts`);
-    console.log(`Counter:${mistakesCounter}`);
-  }
-  if (letterFromUser === randWord) {
-    return alert('Good game! You won!');
-  }
+//   while (lowCaseWord.indexOf(lowCaseLetter) === -1 && mistakesCounter < 3) {
+//     mistakesCounter++;
+//     letterFromUser = prompt(`Nope, try again! You have ${4 - mistakesCounter} attempts`);
+//     console.log(`Counter:${mistakesCounter}`);
+//   }
+//   if (letterFromUser === randWord) {
+//     return alert('Good game! You won!');
+//   }
 
-  if (lowCaseWord.indexOf(lowCaseLetter) >= 0) {
-    letterFromUser = prompt(`Nice! You have guessed`, guessedLetter(randWord, letterFromUser));
-  }
-}
+//   if (lowCaseWord.indexOf(lowCaseLetter) >= 0) {
+//     letterFromUser = prompt(`Nice! You have guessed`, guessedLetter(randWord, letterFromUser));
+//   }
+// }
 
-function validateUserInput(letterFromUser) {
-  while (letterFromUser.toLowerCase() === letterFromUser.toUpperCase()) {
-    letterFromUser = prompt('Invalid input!Only letters!');
-  }
-  isCompatible(letterFromUser, randWord);
-}
+// function validateUserInput(letterFromUser) {
+//   while (letterFromUser.toLowerCase() === letterFromUser.toUpperCase()) {
+//     letterFromUser = prompt('Invalid input!Only letters!');
+//   }
+//   isCompatible(letterFromUser, randWord);
+// }
 
-const game = () => {
-  let offerToGame = confirm('Do you want to play a game?');
-  if (offerToGame === true) {
-    let letterFromUser = prompt('Type letter in');
-    validateUserInput(letterFromUser);
-  }
-};
+// const game = () => {
+//   let offerToGame = confirm('Do you want to play a game?');
+//   if (offerToGame === true) {
+//     let letterFromUser = prompt('Type letter in');
+//     validateUserInput(letterFromUser);
+//   }
+// };
 
-game();
+// game();
+
+
--- lesson5-homework-tasks/index.js
+++ lesson5-homework-tasks/index.js
@@ -1,1 +1,59 @@
-const func = (film, schedule, price) => {};
+/**
+ *
+ * @param {String} title
+ * @param {Array} schedule
+ * @param {Number} ticketPrice
+ * @returns
+ */
+function createMovie(title, schedule, ticketPrice, seets = 32) {
+  /**
+   *
+   * @param {number} discount - number bigger than 0 and less than 1 for calculating new prices
+   * @returns
+   */
+  function makeSale(discount) {
+    if (discount < 0 || discount >= 1) {
+      console.error('Введено не коректне значення, має бути не мешне 0 та не більше 1');
+      return null;
+    }
+    this.ticketPrice = this.ticketPrice * discount;
+  }
+
+  return {
+    title,
+    schedule,
+    ticketPrice,
+    makeSale,
+    seets,
+  };
+}
+
+function cinema(name, address, defaultFilmsStock = []) {
+  const sumAllTickets = function () {
+    let sum = 0;
+    for (let film of this.filmsStock) {
+      sum += film.ticketPrice * film.seets;
+    }
+    return sum;
+  };
+  const releaseFilm = function (filmToRelease) {
+    this.releaseFilm = filmToRelease;
+  };
+  return {
+    name,
+    address,
+    filmsStock: defaultFilmsStock,
+    filmsRelease: null,
+    sumAllTickets,
+    releaseFilm,
+  };
+}
+
+const imaxFilmStore = [
+  createMovie('Matrix', [], 30, 100),
+  createMovie('Iron man', [], 40, 200),
+  createMovie('Wild west', [], 25, 150),
+];
+const imax = cinema('IMAX', 'in the middle of nowhere', imaxFilmStore);
+const allCosts = imax.sumAllTickets();
+console.log(allCosts);
