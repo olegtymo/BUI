dc46c2747fbf0deec9c6b2ed1466977e3aea17be
olegtymo <oleg_tymo@ukr.net>
2023-04-13 20:09:51 +0300

    was made some correctives

--- HW_2/index.js
+++ HW_2/index.js
@@ -1,89 +1,72 @@
-/**
- *
- * @param {String} title
- * @param {Array} schedule
- * @param {Number} ticketPrice
- * @returns
- */
-function createMovie(title, schedule, ticketPrice, seats = 32) {
-  /**
-   *
-   * @param {number} discount - number bigger than 0 and less than 1 for calculating new prices
-   * @returns
-   */
-  function makeSale(discount) {
-    if (discount < 0 || discount >= 1) {
-      console.error('Введено не коректне значення, має бути не мешне 0 та не більше 1');
-      return null;
-    }
-    this.ticketPrice = this.ticketPrice * discount;
-  }
+// ######################################################################  TASK1  #########################################################################
+function without(object, propertyName) {
+  object[propertyName] = null;
+}
+const data = { login: 'gogi', password: 'GloryOfUkraine', address: 'Kiev' };
+console.log(without(data, 'address')); // { login: 'gogi', password: 'GloryOfUkraine', address: null}
 
-  return {
-    title,
-    schedule,
-    ticketPrice,
-    makeSale,
-    seats,
-  };
+// ######################################################################  TASK2  #########################################################################
+
+// /**
+//  *
+//  * @param {string} title
+//  * @param {number} price
+//  * @param {number} amount
+//  */
+function ItemCreator(title, price, amount) {
+  this.title = title;
+  this.price = price;
+  this.amount = amount;
+}
+function StorageItemCreator(title, price, amount) {
+  this.title = title;
+  this.price = price;
+  this.amount = amount;
 }
 
-function cinema(name, address, defaultFilmsStock = []) {
-  /**
-   *
-   * @param {object} movie
-   * @param {string} showTime - format "m.d.y, h:m"
-   * @param {number} weekDuring
-   * @returns
-   */
-  const weekSchedule = new Array();
-
-  function schedule(movie, showTime, weekDuring) {
-    const dateConvertor = new Date(showTime);
-    weekSchedule.push([movie.title, showTime]);
-    this.weekSchedule = weekSchedule;
-  }
-
-  const sumAllTickets = function () {
-    let sum = 0;
-    for (let film of this.filmsStock) {
-      sum += film.ticketPrice * film.seats;
-    }
-    return sum;
-  };
-  const releaseFilm = function (filmToRelease) {
-    this.releaseFilm = filmToRelease;
-  };
-  return {
-    name,
-    address,
-    filmsStock: defaultFilmsStock,
-    filmsRelease: null,
-    sumAllTickets,
-    releaseFilm,
-    schedule,
-  };
-}
-
-const imaxFilmStore = [
-  createMovie('Matrix', [], 30, 100),
-  createMovie('Iron man', [], 40, 200),
-  createMovie('Wild west', [], 25, 150),
+const listItem = [
+  new ItemCreator('Bread', 20, 70),
+  new ItemCreator('Milk', 25, 35),
+  new ItemCreator('Meat', 40, 40),
+  new ItemCreator('Water', 10, 76),
+  new ItemCreator('Chips', 15, 12),
 ];
-const imax = cinema('IMAX', 'in the middle of nowhere', imaxFilmStore);
-const allCosts = imax.sumAllTickets();
 
-const filmRunningMen = createMovie('RunningMen', [], 20, 400);
-const filmMatrix = createMovie('Matrix', [], 30, 100);
-const filmAvengers = createMovie('Avengers', [], 40, 100);
+const storageListItem = [
+  new ItemCreator('Water', 10, 56),
+  new ItemCreator('Bread', 20, 70),
+  new ItemCreator('Milk', 25, 55),
+  new ItemCreator('Meat', 40, 70),
+  new ItemCreator('Chips', 15, 12),
+];
 
-imax.schedule(filmMatrix, 'Wednesday 18:30', 3);
-imax.schedule(filmMatrix, 'Friday 19:00', 3);
-imax.schedule(filmRunningMen, 'Monday 18:40', 2);
-imax.schedule(filmRunningMen, 'Saturday 21:00', 2);
-imax.schedule(filmRunningMen, 'Friday 14:40', 2);
-imax.schedule(filmAvengers, 'Saturday 20:00', 2);
-imax.schedule(filmAvengers, 'Thursday 17:30', 2);
-console.log(imax.weekSchedule);
-
-function showingCounter() {}
+function ProfileStore(label, schedule, products) {
+  this.label = label;
+  this.schedule = schedule;
+  this.products = products;
+  //   console.log(this.products);
+  this.makeBlackFriday = function (discount) {
+    if (discount <= 0 || discount >= 1) {
+      throw new Error('Wrong argument type. Should be > 0 and < 1');
+    }
+    for (let i of this.products) {
+      i.price = i.price * discount;
+    }
+  };
+  this.verifyStore = function (storage) {
+    let mismatchedItems = '';
+    this.products.map((productItem) => {
+      let matchedItem = storage.find((storageItem) => storageItem.title == productItem.title);
+      if (matchedItem.amount !== productItem.amount) {
+        mismatchedItems += `${matchedItem.title}, `;
+        productItem.amount = matchedItem.amount;
+      }
+    });
+    if (mismatchedItems) {
+      mismatchedItems = mismatchedItems.slice(0, -2);
+    } else 'All items are matched';
+    return mismatchedItems;
+  };
+}
+const newStore = new ProfileStore('Market', '07:00 - 18:00', listItem);
+console.log(newStore.verifyStore(storageListItem));
