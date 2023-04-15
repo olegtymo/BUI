109cdefda30d6903fa6333462d9ec80fb4ed3675
olegtymoshenko <oleg.tymo@gmail.com>
2023-04-15 14:50:39 +0300

    TASK3 is completed

--- HW_4/script.js
+++ HW_4/script.js
@@ -1,28 +1,30 @@
 //#############################################################################################   TASK1   ##############################################################################
 
-// const fill = (arraySize, value) => {
-//   let arr = new Array(arraySize).fill(value);
-//   return arr;
-// };
+const fill = (arraySize, value) => {
+  let arr = new Array(arraySize).fill(value);
+  return arr;
+};
 
 //#############################################################################################   TASK2   ##############################################################################
 
-// const reverseArray = (array) => {
-//   const reversedArr = new Array();
-//   for (let i = array.length - 1; i >= 0; i--) {
-//     reversedArr.push(array[i]);
-//   }
-//   return reversedArr;
-// };
+const reverseArray = (array) => {
+  const reversedArr = new Array();
+  for (let i = array.length - 1; i >= 0; i--) {
+    reversedArr.push(array[i]);
+  }
+  return reversedArr;
+};
 
 //#############################################################################################   TASK3   ##############################################################################
 let arr = [1, 3, 5, 7, 9, 11, 12];
 let arr2 = [1, 2, 3, 4, 5, 10, 12];
 
 const sortArr = (firstArray, secondArray) => {
-  let mergedArrays = [].concat(firstArray, secondArray);
-  // const filteredArr = mergedArrays.filter((el, i, arr) => {});
-  return mergedArrays;
+  const mergedArrays = [].concat(firstArray, secondArray).sort((a, b) => a - b);
+  const uniqArray = mergedArrays.reduce((uniq, item) => {
+    return uniq.includes(item) ? uniq : [...uniq, item]
+  }, []);
+  return uniqArray;
 };
 
 console.log(sortArr(arr, arr2));
