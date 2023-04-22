d4f67d52058ee54aed7822b8b5f9c7b1fe87ef80
olegtymoshenko <oleg.tymo@gmail.com>
2023-04-22 23:03:49 +0300

    3/3 tasks are done

---
+++ ExtraPractice[Callback]/script.js
@@ -0,0 +1,40 @@
+// ######################################################################  TASK1  #########################################################################
+const someFunction = (value, success, failure) => {
+  value ? success() : failure()
+}
+
+// ######################################################################  TASK2  #########################################################################
+const locationsMap = {
+  "Суми": ["Охтирка", "Білопілля", "Шостка", "Конотоп"],
+  "Чернігів": ["Новгород-Сіверський"],
+  "Харків": ["Дергачі", "Чугуїв", "Балаклія", "Слобожанське", "Зміїв"],
+  "Київ": ["Яготин", "Бориспіль", "Бровари"]
+}
+
+const getNeighbours = (anyLocation) => {
+  return Object.keys(locationsMap).includes(anyLocation) ? locationsMap[anyLocation] : null;
+}
+
+const waetherApp = (location, getNeighbours) => {
+  const citiesArray = getNeighbours(location);
+  const arrayOfObjects = citiesArray.map((city) => {
+    return {
+      name: city,
+      yearsOld: NaN,
+      population: NaN
+    }
+  })
+  return arrayOfObjects;
+}
+
+// ######################################################################  TASK3  #########################################################################
+const isSpecialCharacter = (value) => {
+  return value.toLowerCase() === value.toUpperCase() ? true : false
+}
+const removeSpecialCharacters = (url, isSpecialCharacter) => {
+  let validatedUrl = '';
+  for (let i of url) {
+    !isSpecialCharacter(i) ? validatedUrl += i : ''
+  }
+  return validatedUrl;
+}
